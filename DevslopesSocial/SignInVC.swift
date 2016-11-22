4dd60b50390d387555546da7f62a19aded00524e
John Kine <johnkine@Johns-MacBook-Pro.local>
2016-11-21 16:25:01 -0800

    Completed setting up firebase integertion

---
+++ DevslopesSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  DevslopesSocial
+//
+//  Created by John Kine on 2016-11-21.
+//  Copyright © 2016 John Kine. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
