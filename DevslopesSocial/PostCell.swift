a26b05ce83ce566758885f66a0a322d6e5b1fa81
John Kine <jkine00@me.com>
2016-11-23 16:41:38 -0800

    Feed UI part 2 complete

---
+++ DevslopesSocial/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  DevslopesSocial
+//
+//  Created by John Kine on 2016-11-23.
+//  Copyright © 2016 John Kine. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg:UIImageView!
+    @IBOutlet weak var usernameLbl:UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption:UITextView!
+    @IBOutlet weak var likesLbl:UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+
+}
